a72abca6145276eb99d5fe0a9da60e67a5f7712c
NeonLights10 <8921618+NeonLights10@users.noreply.github.com>
2019-06-25 00:48:24 -0400

    Make it so that bot doesn't record deleting commands

--- musicbot/bot.py
+++ musicbot/bot.py
@@ -3783,9 +3783,10 @@ class MusicBot(discord.Client):
 
     async def on_message_delete(self, message):
         if not message.author.id == self.user.id:
-            recordChannel = message.guild.get_channel(int(self.config.recordmsg))
-            await self.safe_send_message(recordChannel, "**{}#{}** (ID: {}) message has been deleted from **#{}:**".format(message.author.name, message.author.discriminator, message.author.id, message.channel.name))
-            await self.safe_send_message(recordChannel, "**Message:** {}".format(message.content))
+            if re.match('^{}'.format(self.config.command_prefix), message.content) == None:
+                recordChannel = message.guild.get_channel(int(self.config.recordmsg))
+                await self.safe_send_message(recordChannel, "**{}#{}** (ID: {}) message has been deleted from **#{}:**".format(message.author.name, message.author.discriminator, message.author.id, message.channel.name))
+                await self.safe_send_message(recordChannel, "**Message:** {}".format(message.content))
 
     async def on_message_edit(self, before, after):
         if not before.author.id == self.user.id:
