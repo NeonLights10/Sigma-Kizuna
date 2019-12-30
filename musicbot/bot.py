6fca1485312a341e5ebb73d6926af9fb42760f48
NeonLights10 <8921618+NeonLights10@users.noreply.github.com>
2019-12-30 14:12:03 -0500

    Fix pymongo stuff

--- musicbot/bot.py
+++ musicbot/bot.py
@@ -1345,12 +1345,12 @@ class MusicBot(discord.Client):
             {command_prefix}purgedb
         Adds fields with default values to an existing document if a new field is added in development.
         """
-        await self.dbservers.update({"server_id": guild.id, 'ruleschannel': {$exists : false}}, {$set: {'ruleschannel': None}})
-        await self.dbservers.update({"server_id": guild.id, 'welcomechannel': {$exists : false}}, {$set: {'welcomechannel': None}})
-        await self.dbservers.update({"server_id": guild.id, 'announcementchannel': {$exists : false}}, {$set: {'announcementchannel': None}})
-        await self.dbservers.update({"server_id": guild.id, 'msglog': {$exists : false}}, {$set: {'msglog': None}})
-        await self.dbservers.update({"server_id": guild.id, 'invitelog': {$exists : false}}, {$set: {'invitelog': 'n'}})
-        await self.dbservers.update({"server_id": guild.id, 'welcomemsg': {$exists : false}}, {$set: {'welcomemsg': None}})
+        await self.dbservers.update({"server_id": guild.id, 'ruleschannel': {"$exists": false}}, {"$set": {'ruleschannel': None}})
+        await self.dbservers.update({"server_id": guild.id, 'welcomechannel': {"$exists": false}}, {"$set": {'welcomechannel': None}})
+        await self.dbservers.update({"server_id": guild.id, 'announcementchannel': {"$exists": false}}, {"$set": {'announcementchannel': None}})
+        await self.dbservers.update({"server_id": guild.id, 'msglog': {"$exists": false}}, {"$set": {'msglog': None}})
+        await self.dbservers.update({"server_id": guild.id, 'invitelog': {"$exists": false}}, {"$set": {'invitelog': 'n'}})
+        await self.dbservers.update({"server_id": guild.id, 'welcomemsg': {"$exists": false}}, {"$set": {'welcomemsg': None}})
 
     async def cmd_dbconfig(self, guild, message, channel_mentions, leftover_args, config = None, channelmention = None):
         """
